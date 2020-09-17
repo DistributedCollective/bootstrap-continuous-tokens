d912538827e465dd2cfd5ab52816a5785ef308ff
Alan Verbner <alverbner@gmail.com>
2020-09-17 19:57:41 -0300

    chore: gas usage task

--- buidler.config.js
+++ buidler.config.js
@@ -1,6 +1,7 @@
 usePlugin("@nomiclabs/buidler-waffle");
 usePlugin("@nomiclabs/buidler-solhint");
 usePlugin("solidity-coverage");
+usePlugin("buidler-gas-reporter");
 
 // This is a sample Buidler task. To learn how to create your own go to
 // https://buidler.dev/guides/create-task.html
@@ -16,10 +17,16 @@ task("accounts", "Prints the list of accounts", async () => {
 // This object can have the following optional entries:
 // defaultNetwork, networks, solc, and paths.
 // Go to https://buidler.dev/config/ to learn more
+
 module.exports = {
   // This is a sample solc configuration that specifies which version of solc to use
   solc: {
     version: "0.6.8",
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS ? true : false,
+    currency: "USD",
+    gasPrice: 21,
   },
   networks: {
     coverage: {
