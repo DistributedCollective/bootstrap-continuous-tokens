5387e94f16710ba6463bb3c49498d8c928177094
Alan Verbner <alverbner@gmail.com>
2020-09-18 17:29:53 -0300

    chore: added contract size plugin

--- buidler.config.js
+++ buidler.config.js
@@ -1,6 +1,7 @@
 usePlugin("@nomiclabs/buidler-waffle");
 usePlugin("solidity-coverage");
 usePlugin("buidler-gas-reporter");
+usePlugin('buidler-contract-sizer');
 
 // This is a sample Buidler task. To learn how to create your own go to
 // https://buidler.dev/guides/create-task.html
